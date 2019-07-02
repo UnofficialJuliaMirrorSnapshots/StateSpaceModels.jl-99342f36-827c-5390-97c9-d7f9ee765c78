095b4ab661bced081b79eacbc72cab58643e87ec
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-07-02 01:31:31 +0000

    Snapshot of branch master taken on 2019-07-01T21:31:30.858-04:00 from "https://github.com/UnofficialJuliaMirror/StateSpaceModels.jl-99342f36-827c-5390-97c9-d7f9ee765c78"

--- src/simulation.jl
+++ src/simulation.jl
@@ -14,8 +14,15 @@ function simulate(ss::StateSpace, N::Int, S::Int)
 
     # Load system matrices
     n, p, m, r = size(ss.model)
-    Z, T, R = ztr(ss.model)
-    Z = Z[:, :, 1]
+    Z0, T, R   = ztr(ss.model)
+
+    Z = Array{Float64, 3}(undef, p, m, N)
+    if ss.model.mode == "time-invariant"
+        Z[:, :, 1:N] .= Z0[:, :, 1]
+    else
+        size(Z0, 3) < n+N && error("Time-variant Z too short for simulating $N steps ahead")
+        Z[:, :, 1:N] .= Z0[:, :, n+1:n+N]
+    end
 
     # Load a, P, and F at last in-sample instant
     a0 = ss.smoother.alpha[end, :]
@@ -32,16 +39,16 @@ function simulate(ss::StateSpace, N::Int, S::Int)
 
     # Initialization
     a[1, :]    = T*a0
-    P[:, :, 1]    = T*P0*T' + R*Q*R'
-    F[:, :, 1]    = Z*P[:, :, 1]*Z' + H
-    dist[1] = MvNormal(vec(Z*a[1, :]), Symmetric(F[:, :, 1]))
-    sim = Array{Float64, 3}(undef, ss.model.dim.p, N, S)
+    P[:, :, 1] = T*P0*T' + R*Q*R'
+    F[:, :, 1] = Z[:, :, 1]*P[:, :, 1]*Z[:, :, 1]' + H
+    dist[1]    = MvNormal(vec(Z[:, :, 1]*a[1, :]), Symmetric(F[:, :, 1]))
+    sim        = Array{Float64, 3}(undef, ss.model.dim.p, N, S)
 
     for t = 2:N
-        a[t, :] = T*a[t-1, :]
+        a[t, :]    = T*a[t-1, :]
         P[:, :, t] = T*P[:, :, t-1]*T' + R*Q*R'
-        F[:, :, t] = Z*P[:, :, t]*Z' + H
-        dist[t] = MvNormal(vec(Z*a[t, :]), Symmetric(F[:, :, t]))
+        F[:, :, t] = Z[:, :, t]*P[:, :, t]*Z[:, :, t]' + H
+        dist[t]    = MvNormal(vec(Z[:, :, t]*a[t, :]), Symmetric(F[:, :, t]))
     end
 
     for t = 1:N
